f3fc87f1f537dfb70ff827bd109601d22299ebc1
ke975 <60978634+ke975@users.noreply.github.com>
2021-06-25 15:09:02 -0600

    cambio de colores

--- cow/screens/Home.js
+++ cow/screens/Home.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import { ScrollView,Text} from 'react-native';
+import { ScrollView,Text,StyleSheet} from 'react-native';
 
 // import { Container } from './styles';
 
 const Home = () => {
   return(
-<ScrollView > 
-<Text>hola muundo</Text>
+<ScrollView style={styles.container} > 
+<Text style={StyleSheet.Text}>hola muundo</Text>
 
 </ScrollView>
   )
 }
 
+const styles= StyleSheet.create({
+container:{
+  flex:1,
+  margin:20
+},
 
+Text:{
+  justifyContent:'center',
+  alignItems:'center'
+}
+
+
+
+})
 
 
 export default Home;
